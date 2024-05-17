73b808b9f204fc2539d5d1f59853da3fdc055108
CHAPAPOPA <yackow.muliawin2015@yandex.ru>
2024-05-17 22:43:13 +0300

    hw_5 (external changes)

--- blog/models.py
+++ blog/models.py
@@ -5,7 +5,7 @@ class Blog(models.Model):
     title = models.CharField(max_length=100, verbose_name="заголовок")
     slug = models.CharField(max_length=100, verbose_name="slug", null=True, blank=True)
     content = models.TextField(verbose_name='содержимое')
-    image = models.ImageField(verbose_name='изображение', blank=True, null=True)
+    image = models.ImageField(verbose_name='изображение', blank=True, null=True, upload_to="blog/image")
     created_at = models.DateTimeField(auto_now_add=True, verbose_name='дата и время создания')
     is_published = models.BooleanField(default=True, verbose_name='опубликовано')
     views_count = models.PositiveIntegerField(verbose_name='просмотры', default=0)
