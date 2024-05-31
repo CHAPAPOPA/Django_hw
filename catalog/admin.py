98c7c81bb9b3a7f71e08e09bb7b86d6e90ecfa37
CHAPAPOPA <yackow.muliawin2015@yandex.ru>
2024-05-31 13:58:44 +0300

    hw_6 not the full version

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from catalog.models import Category, Product
+from catalog.models import Category, Product, Version
 
 
 @admin.register(Category)
@@ -11,4 +11,11 @@ class CategoryAdmin(admin.ModelAdmin):
 class ProductAdmin(admin.ModelAdmin):
     list_display = ("id", "name", "category", "purchase_price")
     list_filter = ("category",)
+    search_fields = ("name",)
+
+
+@admin.register(Version)
+class VersionAdmin(admin.ModelAdmin):
+    list_display = ("id", "product", "number", "name", "is_active")
+    list_filter = ("product", "is_active")
     search_fields = ("name",)
